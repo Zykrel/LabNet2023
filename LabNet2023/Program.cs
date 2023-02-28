65dc55b0837c249c44e3a15f89ff1dd78135eebf
Nahuel Garcia <kamaelnahuel@gmail.com>
2023-02-28 00:42:36 -0300

    Agrego HelloWorld

--- LabNet2023/Program.cs
+++ LabNet2023/Program.cs
@@ -9,7 +9,10 @@ namespace LabNet2023
     internal class Program
     {
         static void Main(string[] args)
+
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine(); //Utilizo ReadLine para que no se cierre la consola.
         }
     }
 }
