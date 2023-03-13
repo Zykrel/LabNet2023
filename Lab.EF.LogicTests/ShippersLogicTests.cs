bcd6d1e14deb193b7fdf5e92e3d81bed130a2452
Nahuel Garcia <kamaelnahuel@gmail.com>
2023-03-13 20:57:10 -0300

    Creacion de Unit test

---
+++ Lab.EF.LogicTests/ShippersLogicTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab.EF.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab.EF.Entities;
+
+namespace Lab.EF.Logic.Tests
+{
+    [TestClass()]
+    public class ShippersLogicTests
+    {
+        [TestMethod()]
+        public void ObtenerTodoTest()
+        {
+            ShippersLogic shipperLogic = new ShippersLogic();
+
+            Assert.IsTrue(shipperLogic.ObtenerTodo().Any());
+        }
+    }
+}
