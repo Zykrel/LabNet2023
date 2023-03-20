5568f6e54d792246dada5c36503f20aa11bdc9a1
Nahuel Garcia <kamaelnahuel@gmail.com>
2023-03-20 00:36:34 -0300

    correcciones de codigo

--- js/logica.js
+++ js/logica.js
@@ -1,6 +1,6 @@
         let puntajeMaximo = 0;
         let puntajeActual = 10;
-        let numeroAleatorio = Math.floor(Math.random() * 10) + 1;
+        let numeroAleatorio = Math.floor(Math.random() * 10) +1;
 
         const adivinarNumero = () => {
             let numeroIngresado = document.getElementById("numero").value;
@@ -8,19 +8,29 @@
                 document.getElementById("resultado").innerHTML = "¡Ganaste!";
                 puntajeActual += 5;
                 if (puntajeActual > puntajeMaximo) {
-                    puntajeMaximo = puntajeActual;
+                     puntajeMaximo = puntajeActual;
                 }
                 document.getElementById("puntaje").innerHTML = "Puntaje actual: " + puntajeActual + " - Puntaje máximo: " + puntajeMaximo;
-                numeroAleatorio = Math.floor(Math.random() * 10) + 1;
-            } else {
-                document.getElementById("resultado").innerHTML = "No adivinaste, intenta de nuevo";
+                numeroAleatorio = Math.floor(Math.random() * 10) +1;
+            } else if( numeroAleatorio < numeroIngresado) {
+                document.getElementById("resultado").innerHTML = "No adivinaste, intenta de nuevo. Pista: El numero es menor al ingresado";
                 puntajeActual -= 1;
                 if (puntajeActual == 0) {
-                    document.getElementById("resultado").innerHTML = "Perdiste";
+                    document.getElementById("resultado").innerHTML = "Perdiste. El numero es: " + numeroAleatorio;
                     puntajeActual = 10;
                     puntajeMaximo = 0;
                 }
                 document.getElementById("puntaje").innerHTML = "Puntaje actual: " + puntajeActual + " - Puntaje máximo: " + puntajeMaximo;
+            }else{
+                document.getElementById("resultado").innerHTML = "No adivinaste, intenta de nuevo. Pista: El numero es mayor al ingresado";
+                puntajeActual -= 1;
+                if (puntajeActual == 0) {
+                    document.getElementById("resultado").innerHTML = "Perdiste. El numero es: " + numeroAleatorio;
+                    puntajeActual = 10;
+                    puntajeMaximo = 0;
+                }
+                document.getElementById("puntaje").innerHTML = "Puntaje actual: " + puntajeActual + " - Puntaje máximo: " + puntajeMaximo;
+
             }
         }
  
