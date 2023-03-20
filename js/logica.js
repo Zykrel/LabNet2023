1e5e0c845d9c02d418df60a16e43835acc4ec50b
Nahuel Garcia <kamaelnahuel@gmail.com>
2023-03-20 01:46:59 -0300

    Cambios en bootstrap

--- js/logica.js
+++ js/logica.js
@@ -1,4 +1,4 @@
-let puntajeA = 20;
+let puntajeA = 10;
 let puntajeMaximo = 0;
 let numeroAleatorio = Math.floor(Math.random() * 20) + 1;
 
@@ -11,7 +11,7 @@ const adivinarNumero = () => {
     let numeroIngresado = parseInt(document.getElementById("numero").value);
 
     if (numeroIngresado == numeroAleatorio) {
-        document.getElementById("resultado").innerHTML = "¡Ganaste! ---- Para continuar elija otro numero";
+        document.getElementById("resultado").innerHTML = "¡Ganaste!" + " Para continuar elija otro número";
         puntajeA += puntajeMaximo;
         if (puntajeA > puntajeMaximo) {
             puntajeMaximo = puntajeA;
@@ -19,22 +19,22 @@ const adivinarNumero = () => {
             cambiarNumeroA();
         }
     } else if (numeroAleatorio < numeroIngresado) {
-        document.getElementById("resultado").innerHTML = "No adivinaste, intenta de nuevo. Pista: El numero es menor al ingresado";
+        document.getElementById("resultado").innerHTML = "No adivinaste, intenta de nuevo. \n Pista: El número es menor al ingresado";
         puntajeA -= 1;
         if (puntajeA == 0) {
             document.getElementById("resultado").innerHTML = "Perdiste. El numero es: " + numeroAleatorio;
-            puntajeA = 20;
+            puntajeA = 10;
             puntajeMaximo = 0;
             cambiarNumeroA();
 
         }
         document.getElementById("puntaje").innerHTML = "Puntaje actual: " + puntajeA + " - Puntaje máximo: " + puntajeMaximo;
     } else {
-        document.getElementById("resultado").innerHTML = "No adivinaste, intenta de nuevo. Pista: El numero es mayor al ingresado";
+        document.getElementById("resultado").innerHTML = "No adivinaste, intenta de nuevo. Pista: El número es mayor al ingresado";
         puntajeA -= 1;
         if (puntajeA == 0) {
-            document.getElementById("resultado").innerHTML = "Perdiste, te quedaste sin intentos. El numero era: " + numeroAleatorio;
-            puntajeA = 20;
+            document.getElementById("resultado").innerHTML = "Perdiste, te quedaste sin intentos. El número era: " + numeroAleatorio;
+            puntajeA = 10;
             puntajeMaximo = 0;
             cambiarNumeroA();
         }
